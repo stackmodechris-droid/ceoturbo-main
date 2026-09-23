33aa003876bd6b38adcaf7eb8fbb189245f59eac
stackmodechris-droid <stackmodechris@gmail.com>
2026-09-23 00:53:48 -0400

    Update Google Analytics tag to G-KYZ33MZKJH

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="en" className={`${archivo.variable} ${source.variable}`}>
       <head>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-J6BG8YXRZB"
+          src="https://www.googletagmanager.com/gtag/js?id=G-KYZ33MZKJH"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -58,7 +58,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-J6BG8YXRZB');
+            gtag('config', 'G-KYZ33MZKJH');
           `}
         </Script>
       </head>
